76c021ef563eb7d89ced7982589980ea11db7421
artem lobanov <120090180+zkittels@users.noreply.github.com>
2023-02-09 03:23:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let snake_case = 10;
+const width = 11;
+console.log(snake_case)
+alert("Hello world")
+let title = 10;
+console.log(title)
+let screens = 10;
+console.log(screens)
+let screenPrice = 10;
+console.log(screenPrice)
+let rollback = 10;
+console.log(rollback)
+let fullPrice = 10;
+console.log(fullPrice)
+let adaptive = 10;
+console.log(adaptive)
+myVar = Symbol();
+console.log (typeof myVar)
